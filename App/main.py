6c7b57b65b5337813037a008f0b41ff3afff52cc
Otrek Wilke <otrek.wilke@datanomiq.de>
2022-08-15 17:29:41 +0200

    a very basic cloud run app

---
+++ App/main.py
@@ -0,0 +1,22 @@
+from distutils.log import debug
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    if requesst.method == 'POST':
+        # get some incomming data
+        data = request.get_json()
+        return f"Hello {data['name']}"
+    return "Hello World"
+
+if __name__ == '__main__':
+    """
+    Creating a very basic debug app. Don't use this in production
+    """
+    app.run(
+        debug=True,
+        port=8000,
+        host="0.0.0.0"
+    )
